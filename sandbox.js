812e44015f3c1224486251b2fc1de413653ce32e
GentleMizt <olamideiyanda18@gmail.com>
2023-01-31 20:23:57 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -9,9 +9,3 @@ bottom.textContent = currentNumber;
 flipCard.dataset.currentNumber = currentNumber;
 flipCard.dataset.nextNumber = currentNumber - 1;
 flipCard.classList.add('flip');
-flipCard.addEventListener('animationstart');
-flipCard.addEventListener('animationend');
-
-start  =>{
-
-}
