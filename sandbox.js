4ac7a0bb8c286761651e677c77994b82ac988ceb
GentleMizt <olamideiyanda18@gmail.com>
2023-01-31 20:11:28 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,17 @@
 const currentNumber = 9;
 const flipCard = document.querySelector('.flip-card');
-[...flipCard.children].forEach(child => {
-    child.textContent = currentNumber
-});
+const top = flipCard.querySelector('.top');
+const bottom = flipCard.querySelector('.bottom');
+
+top.textContent = currentNumber;
+bottom.textContent = currentNumber;
+
 flipCard.dataset.currentNumber = currentNumber;
 flipCard.dataset.nextNumber = currentNumber - 1;
 flipCard.classList.add('flip');
-flipCard.addEventListener('transitionstart')
-flipCard.addEventListener('transitionend')
+flipCard.addEventListener('animationstart');
+flipCard.addEventListener('animationend');
+
+start  =>{
+
+}
