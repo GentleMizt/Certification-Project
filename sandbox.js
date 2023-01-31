797419e629eac3712dc5773dcbb64804c27826c1
GentleMizt <olamideiyanda18@gmail.com>
2023-01-31 20:00:42 +0100

    progress

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+const defNum = 9;
+const flipCard = document.querySelector('.flip-card');
+[...flipCard.children].forEach(child => {
+    child.textContent = defNum
+});
+flipCard.dataset.currentNumber = defNum;
+flipCard.dataset.nextNumber = defNum - 1;
+flipCard.classList.add('flip');
