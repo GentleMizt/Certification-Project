16d3fc049f871b5c552182ce33dd15b186f7649e
GentleMizt <olamideiyanda18@gmail.com>
2023-01-31 21:20:02 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -2,7 +2,10 @@ const currentNumber = 9;
 const flipCard = document.querySelector('.flip-card');
 const topHalf = flipCard.querySelector('.top');
 const bottomHalf = flipCard.querySelector('.bottom');
-
+const topFlip = document.createElement('div')
+topFlip.classList.add('top-flip');
+const bottomFlip = document.createElement('div')
+bottomFlip.classList.add('bottom-flip');
 top.textContent = currentNumber;
 bottom.textContent = currentNumber;
 
