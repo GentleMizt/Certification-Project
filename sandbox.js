d3680a78db691c3f1e1c73c8bcb68db7581c97ca
GentleMizt <olamideiyanda18@gmail.com>
2023-02-01 23:57:06 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,19 @@
 const countDown = () =>{
-    const oldDate = new Date('Dec 25, 2022 00:00:00').getTime();
+    const oldDate = new Date('Jan 31, 2023 00:00:00').getTime();
     const now = new Date().getTime();
     const diff = now - oldDate;
 
-    console.log(diff);
+    const secs = 1000;
+    const mins = secs * 60;
+    const hrs =  mins * 60;
+    const days = hrs * 24;
+
+    const timeDays = Math.floor(diff / days);
+    const timeHours = Math.floor((diff % days) / hrs);
+    const timeMinutes = Math.floor((diff % hrs) / mins)
+    const timeSeconds = Math.floor((diff % mins) / secs)
+
+    
+    console.log(timeDays, timeHours, timeMinutes, timeSeconds);
 }
 countDown();
