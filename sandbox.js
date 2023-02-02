2582f3d9092baad923554d10235e892c37cb9106
GentleMizt <olamideiyanda18@gmail.com>
2023-02-03 00:29:20 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -27,16 +27,14 @@ const countDown = () =>{
 }
 countDown();
 
-faceBook.addEventListener('click', ()=>{
-    faceBook.src = 'pathfigma.png';
-})
-let faceBook = document.getElementById('facebook');
-    faceBook.onmouseover = mouseOver;
-    faceBook.onmouseout = mouseOut;
-    
-    function mouseOver() { 
-      faceBook.src = "Pathsvg.svg";
-    }
-    function mouseOut() { 
-      faceBook.src = "icon-facebook.svg";
-    }
+
+// let faceBook = document.getElementById('facebook');
+// faceBook.onmouseover = mouseOver;
+// faceBook.onmouseout = mouseOut;
+
+// function mouseOver() { 
+//     faceBook.src = "Pathsvg.svg";
+// }
+// function mouseOut() { 
+//     faceBook.src = "icon-facebook.svg";
+// }
